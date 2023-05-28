bbb221f1a9cc5595286d5d3bbb8efd20644ad3de
Alaa Younsi <alaayounsi777@gmail.com>
2023-05-28 22:28:40 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+
+  $(document).ready(function(){
+
+    $(".menu-bar").click(function(){
+        $("#Menu").fadeIn(1);
+      });
+  });
+  
+  $(document).ready(function(){
+
+    $(".service-bar").click(function(){
+        $("#Menu").fadeOut(1);
+      });
+  });
+
+  
+  $(document).ready(function(){
+
+    $(".avis-bar").click(function(){
+        $("#Menu").fadeOut(1);
+      });
+  });
+
+  
+  $(document).ready(function(){
+
+    $(".order-bar").click(function(){
+        $("#Menu").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $(".service-bar").click(function(){
+        $("#Service").fadeIn(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $(".menu-bar").click(function(){
+        $("#Service").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $(".avis-bar").click(function(){
+        $("#Service").fadeOut(1);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $(".order-bar").click(function(){
+        $("#Service").fadeOut(1);
+      });
+  });
